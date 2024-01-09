77d00846b6d6cc7b37111aeb04f2a8ab94713cf6
Nima Pourjafar <nima.pourjafar123@gmail.com>
2024-01-09 12:19:45 -0500

    package agent into class (todo: add memory)

--- agent.py
+++ agent.py
@@ -10,31 +10,13 @@ from dotenv import load_dotenv
 from example_query import few_shots
 from langchain.agents.agent_toolkits import create_retriever_tool
 import os
+
 load_dotenv()
-
-db = SQLDatabase.from_uri("postgresql://nba_sql:nba_sql@localhost:5432/nba")
-
-llm = ChatOpenAI(model_name="gpt-4", temperature=0)
-embeddings = OpenAIEmbeddings()
-
-few_shot_docs = [
-    Document(page_content=question, metadata={"sql_query": few_shots[question]})
-    for question in few_shots.keys()
-]
-vector_db = FAISS.from_documents(few_shot_docs, embeddings)
-retriever = vector_db.as_retriever()
-
 
 tool_description = """
 This tool will help you understand similar examples to adapt them to the user question.
 Input to this tool should be the user question.
 """
-
-retriever_tool = create_retriever_tool(
-    retriever, name="sql_get_similar_examples", description=tool_description
-)
-custom_tool_list = [retriever_tool]
-
 
 custom_suffix = """
 I should first get the similar examples I know.
@@ -44,15 +26,36 @@ Then I should query the schema of the most relevant tables.
 """
 
 
-agent_executor = create_sql_agent(
-    llm=llm,
-    toolkit=SQLDatabaseToolkit(db=db, llm=llm),
-    verbose=True,
-    agent_type=AgentType.OPENAI_FUNCTIONS,
-    extra_tools=custom_tool_list,
-    suffix=custom_suffix,
-)
+class Agent:
+    def __init__(self, verbose=True, open_ai_model="gpt-4") -> None:
+        self.db = SQLDatabase.from_uri(os.getenv("DATABASE_URI"))
+        self.llm = ChatOpenAI(model_name=open_ai_model, temperature=0)
+        self.embeddings = OpenAIEmbeddings()
+        few_shot_docs = [
+            Document(page_content=question, metadata={"sql_query": few_shots[question]})
+            for question in few_shots.keys()
+        ]
 
-agent_executor.invoke(
-    {"input": "What is Lamelo Balls best game in 2023?", "chat_history": []}
-)
+        vector_db = FAISS.from_documents(few_shot_docs, self.embeddings)
+        retriever = vector_db.as_retriever()
+
+        self.retriever_tool = create_retriever_tool(
+            retriever, name="sql_get_similar_examples", description=tool_description
+        )
+        self.custom_tool_list = [self.retriever_tool]
+        
+        self.agent_executor = create_sql_agent(
+            llm=self.llm,
+            toolkit=SQLDatabaseToolkit(db=self.db, llm=self.llm),
+            verbose=verbose,
+            agent_type=AgentType.OPENAI_FUNCTIONS,
+            extra_tools=self.custom_tool_list,
+            suffix=self.custom_suffix,
+        )
+        
+
+    def invoke(self, input):
+        return self.agent_executor.invoke(
+            {"input": input}
+        )
+        
