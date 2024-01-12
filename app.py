af6b49957d94c2d32390b236336ad4acdb37bf02
Nima Pourjafar <nima.pourjafar123@gmail.com>
2024-01-12 14:58:26 -0500

    added more descriptions and deleted .hypothesis

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ agent = Agent(verbose=True)
 
 st.write("""
 # NBA-GPT
-An open-source alternative to statmuse
+## An open-source alternative to statmuse
 """)
+
+st.write("Data only goes back to 1997 — to December 2023")
+st.write("No awards or all-star games as well")
 
 input = st.text_input("Ask a question about the NBA", key="input")
 
